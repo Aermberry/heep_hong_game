bbd71125fa3631c288eb26312d048f0bca5edbeb
Hayden <haydenchung@ymail.com>
2021-09-15 14:23:36 +0800

    fix game 3 modal failed to provide default gameStage

--- src/games/game_3/Model.js
+++ src/games/game_3/Model.js
@@ -7,7 +7,7 @@ export default class Model {
     }
 
     get gameStage() {
-        if(this._gameStage === null) return this._gameData[0].gameStage;
+        if(typeof this._gameStage === 'undefined' || this._gameStage == null) return this._gameData[0].gameStage;
         return this._gameStage;
     }
 

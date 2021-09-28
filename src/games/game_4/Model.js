3c7eb956e00b5b40fbd93aabb4ed10e281e98e9c
Hayden <haydenchung@ymail.com>
2021-09-28 18:41:18 +0800

    game_4 init

---
+++ src/games/game_4/Model.js
@@ -0,0 +1,25 @@
+export default class Model {
+
+    constructor() {
+        this._gameStage = null;
+        this._gameIndex = 0;
+
+    }
+
+    get gameStage() {
+        if(typeof this._gameStage === 'undefined' || this._gameStage == null) return this._gameData[0].gameStage;
+        return this._gameStage;
+    }
+
+    set gameStage(stage) {
+
+        this._gameStage = stage;
+        this._gameData.some((gameData, ind)=> {
+            if(gameData.gameStage == stage) return this._gameIndex = ind;
+
+        });
+
+    }
+
+
+}
---
+++ src/games/game_4/index.js
@@ -0,0 +1,52 @@
+import Phaser from 'phaser'
+import config from './config'
+
+import Model from './Model'
+import BootScene from './scenes/BootScene'
+
+const gameConfig = Object.assign(config, {
+    scene: [BootScene, PreloaderScene, TutorSecene, GameScene, EndScene]
+});
+
+/**
+ * 
+ * @param {*} loader 
+ * 
+ * An override to fix game audio not playing in mobile device.
+ * https://github.com/photonstorm/phaser/issues/5696
+ * 
+ */
+Phaser.Scenes.SceneManager.prototype.loadComplete = function (loader) {
+    const scene = loader.scene
+    if (this.game.sound && this.game.sound.onBlurPausedSounds) {
+        this.game.sound.unlock()
+    }
+    this.create(scene)
+}
+
+class Game4 extends Phaser.Game {
+
+    // private globals: { model: Model }
+
+    constructor(config, urlParams) {
+        super(config);
+        let model = new Model()
+        this.globals = {
+            model
+        }
+
+        this.globals.model.gameStage = urlParams.sid;
+    }
+}
+
+
+function launch(urlParams) {
+
+    let game = new Game4(gameConfig, urlParams)
+
+    return game
+}
+
+
+export default launch
+export { launch }

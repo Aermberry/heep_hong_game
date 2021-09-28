d21b811b39693d4a07e81403d39677da8fd374d3
Salmon <yshpeng@liangzhicn.com>
2021-09-28 11:29:12 +0800

    update

--- src/games/game_11/scenes/GameScene.js
+++ src/games/game_11/scenes/GameScene.js
@@ -161,16 +161,6 @@ export default class GameScene extends BasicScene {
         return container
     }
 
-
-    // produceToothYValue(x) {
-    //     const a = 542.923767776076;
-    //     const b = 0.874873444090501;
-    //     const c = -0.00129911976352851;
-    //     const d = 7.26209822672699E-07;
-    //     const e = -1.38062230433894E-10;
-    //     return a + b * x + c * x ** 2 + d * x ** 3 + e * x ** 4
-    // }
-
     produceToothYValue(value) {
         let x = value / 1920;
         const a = 1.09723441576348;

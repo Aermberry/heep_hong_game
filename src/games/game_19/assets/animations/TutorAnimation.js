451390b445f8e693b2bfbbedb5b1032a6a5e8091
Salmon <yshpeng@liangzhicn.com>
2021-12-22 15:27:45 +0800

    update: replace tutor  animation

--- src/games/game_19/assets/animations/TutorAnimation.js
+++ src/games/game_19/assets/animations/TutorAnimation.js
@@ -5,10 +5,24 @@
         */
 export const createTutorAnimations = (anims) => {
     anims.create({
-        key: 'tutor_Animation',
+        key: 'tutor01Animation',
         delay: 200,
-        frames: anims.generateFrameNames('tutorTexture', { prefix: 'tut', start: 0, end: 39, zeroPad: 4 }),
-        // frameRate: 20,
-        repeat: -1,
+        frames: anims.generateFrameNames('tutorTexture01', { prefix: 'animationTutor_', start: 0, end: 39, zeroPad: 4 }),
+        repeat: 0,
     });
+
+    anims.create({
+        key: 'tutor02Animation',
+        delay: 200,
+        frames: anims.generateFrameNames('tutorTexture02', { prefix: 'animationTutor_', start: 0, end: 39, zeroPad: 4 }),
+        repeat: 0,
+    });
+
+    anims.create({
+        key: 'tutor03Animation',
+        delay: 200,
+        frames: anims.generateFrameNames('tutorTexture03', { prefix: 'animationTutor_', start: 0, end: 39, zeroPad: 4 }),
+        repeat: 0,
+    });
+  
 }

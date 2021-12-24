f512fbd452253cc88026b76f1bf1b92095740668
Hayden <haydenchung@ymail.com>
2021-12-24 15:22:47 +0800

    setup date for Kids Protected Mode

---
+++ src/games/game_5/scenes/PauseScene.js
@@ -0,0 +1,36 @@
+import { Scene } from 'phaser'
+
+export default class BootScene extends Scene {
+  
+  constructor () {
+    super('Pause');
+  }
+
+  preload () {
+
+    this.load.image('bg_title', require('../assets/Title.png'))
+
+  }
+
+  create () {    
+
+    //已經連續遊玩超過15分鐘了
+    //Begin time
+    //update on landing gaming page, 
+    //if last update is not exist or last update is 15 minute before current time, 
+    //on press resume
+    //update implement in vue
+
+
+
+    //Last update time
+    //Update on every 30 seconds, update implement in vue
+    
+
+
+    //if last update time is 15 minutes larger than begin time, scene.pause
+
+
+
+  }
+}

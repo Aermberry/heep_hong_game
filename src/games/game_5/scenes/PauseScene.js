b2f78d278a7842eceaa98235d74be0a6239e75ff
Hayden <haydenchung@ymail.com>
2021-12-28 14:59:15 +0800

    game1 fix bug

--- src/games/game_5/scenes/PauseScene.js
+++ src/games/game_5/scenes/PauseScene.js
@@ -1,19 +1,35 @@
-import { Scene } from 'phaser'
+import BasicScene from './BasicScene'
+import BasicBtn from '../objects/BasicBtn';
 
-export default class BootScene extends Scene {
+export default class PauseScene extends BasicScene {
   
   constructor () {
     super('Pause');
   }
 
+  init() {
+    this.gamePauseService = typeof this.sys.game.globals.gamePauseService === 'object' ? this.sys.game.globals.gamePauseService : null ;
+  }
+
   preload () {
 
-    //Bg image need to replace with a universalize image
-    this.load.image('bg_title', require('../assets/Title.png'))
+    //Bg and btn image need to replace with a universalize image
+    this.load.image('pause_bg', require('../assets/Title.png'))
+    this.load.spritesheet('cfmBtn', require('../assets/btn_cfm.png'),{ frameWidth: 917, frameHeight: 233 });
 
   }
 
-  create () {    
+  create () {
+
+    this.buildBg('pause_bg')
+
+
+    if(this.gamePuaseService) {
+        this.resumeBtn = new BasicBtn(this, this.getColWidth(6), this.getRowHeight(6));
+        //Button image may also need to provide a universalize image
+        this.resumeBtn.create('cfmBtn', typeof this.gamePuaseService.resumeLastScene === 'function' ? this.gamePauseService.resumeLastScene: ()=> { this.scene.scene.start('Preloader') })
+        this.add.existing(this.resumeBtn)
+    }
 
     //已經連續遊玩超過15分鐘了
     //Begin time
@@ -21,7 +37,7 @@ export default class BootScene extends Scene {
     //if last update is not exist or last update is 15 minute before current time, 
     //on press resume
     //update implement in vue
-
+    
 
 
     //Last update time
@@ -34,5 +50,10 @@ export default class BootScene extends Scene {
 
 
 
+  }
+
+  resumeGameCallback() {
+    const currentPausedScene = this.gamePauseService.getCurrentPausedScene()
+    this.scene.scene.resume(currentPausedScene)
   }
 }

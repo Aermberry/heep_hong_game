7a2149aa3dc1a0d2ea4bc8a4b28e0ec2a12ff784
lcheng <lcheng@liangzhicn.com>
2022-01-24 14:47:24 +0800

    no message

--- src/games/game_29/objects/Article.js
+++ src/games/game_29/objects/Article.js
@@ -9,7 +9,7 @@ export default class Article {
         }
         this.level = level;
         //text_bg scale 0.5 一行容纳30字 竖直 一行容纳15字
-        this.sprite = this.scene.add.sprite(x, y, 'new_text_bg').setOrigin(0).setScale(0.52)
+        this.sprite = this.scene.add.sprite(x, y, 'new_text_bg').setOrigin(0).setScale(0.525)
         this.sprite.width = this.sprite.displayWidth
         this.sprite.height = this.sprite.displayHeight
         this.highlight;
@@ -32,13 +32,13 @@ export default class Article {
         let row = 0;
         let secondeArr = [];
         let lastArr = [];
-        let rowMaxChat = 15;  //横着30字 竖直文本框15字
+        let rowMaxChat = 14;  //横着30字 竖直文本框15字
         this.group = this.scene.add.group();
         data.forEach((word, i) => {
             if (i % rowMaxChat == 0) {
                 row++
             }
-            let characters = this.scene.add.text(originX + i % rowMaxChat * 55, originY + row * 75, word.text.trim(), {
+            let characters = this.scene.add.text(originX + i % rowMaxChat * 60, originY + row * 75, word.text.trim(), {
                 fontSize: '55px', //30px
                 color: word['z-index'][0] == 0 ? '#000000' : "#949494",
                 padding: {
@@ -75,7 +75,7 @@ export default class Article {
     secondeRender(data) {
         let originX = this.oring.x + 35
         let originY = this.oring.y + 40
-        let rowMaxChat = 15;  //横着30字 竖直文本框15字
+        let rowMaxChat = 14;  //横着30字 竖直文本框15字
 
         let arr = [];
         data.forEach((item, i) => {
@@ -106,25 +106,41 @@ export default class Article {
                 this.group.getChildren()[v.index].type += type;
             })
             //将每一行的文字生成
-            let group = arr.map((item) => {
-                return {
-                    index: item[0].index,
-                    row: item[0].row,
-                    text: item.map((item) => item.data.text).join('')
-                }
-            })
+            // let group = arr.map((item) => {
+            //     return {
+            //         index: item[0].index,
+            //         row: item[0].row,
+            //         text: item.map((item) => item.data.text).join('')
+            //     }
+            // })
 
-            group.forEach((item) => {
-                new DragText(this.scene, originX + item.index % rowMaxChat * 55, originY + item.row * 75, item.text, {
-                    fontSize: '55px',
-                    color: "#000000",
-                    backgroundColor: '#E882A4',
-                    padding: {
-                        x: 5, y: 5
-                    },
-                    fontWeight: 'bold',
-                    fontFamily: "system-ui"
-                }, type, 2, group.map((item) => item.text).join(''), this.level != 1)
+            // group.forEach((item) => {
+            //     new DragText(this.scene, originX + item.index % rowMaxChat * 55, originY + item.row * 75, item.text, {
+            //         fontSize: '55px',
+            //         color: "#000000",
+            //         backgroundColor: '#E882A4',
+            //         padding: {
+            //             x: 5, y: 5
+            //         },
+            //         fontWeight: 'bold',
+            //         fontFamily: "system-ui"
+            //     }, type, 2, group.map((item) => item.text).join(''), this.level != 1)
+            // })
+
+            arr.forEach((value) => {
+                value.forEach((item) => {
+                    new DragText(this.scene, originX + item.index % rowMaxChat * 60, originY + item.row * 75, item.data.text, {
+                        fontSize: '55px',
+                        color: "#000000",
+                        backgroundColor: '#E882A4',
+                        padding: {
+                            x: 5, y: 5
+                        },
+                        fontWeight: 'bold',
+                        fontFamily: "system-ui"
+                    }, type, 2, arr.map((value) => value.map((item) => item.data.text).join('')).join(''), this.level != 1)
+                })
+
             })
 
         })
@@ -133,7 +149,7 @@ export default class Article {
     lastRender(data) {
         let originX = this.oring.x + 40
         let originY = this.oring.y + 45
-        let rowMaxChat = 15;
+        let rowMaxChat = 14;
         let arr = [];
         data.forEach((item, i) => {
             if (i != 0) {
@@ -164,25 +180,40 @@ export default class Article {
                 this.group.getChildren()[v.index].type += type;
             })
             //将每一行的文字生成
-            let group = arr.map((item) => {
-                return {
-                    index: item[0].index,
-                    row: item[0].row,
-                    text: item.map((item) => item.data.text).join('')
-                }
-            })
+            // let group = arr.map((item) => {
+            //     return {
+            //         index: item[0].index,
+            //         row: item[0].row,
+            //         text: item.map((item) => item.data.text).join('')
+            //     }
+            // })
 
-            group.forEach((item) => {
-                new DragText(this.scene, originX + item.index % rowMaxChat * 55, originY + item.row * 75, item.text, {
-                    fontSize: '55px',
-                    color: "#000000",
-                    backgroundColor: '#83A9E5',
-                    fontWeight: 'bold',
-                    padding: {
+            // group.forEach((item) => {
+            //     new DragText(this.scene, originX + item.index % rowMaxChat * 55, originY + item.row * 75, item.text, {
+            //         fontSize: '55px',
+            //         color: "#000000",
+            //         backgroundColor: '#83A9E5',
+            //         fontWeight: 'bold',
+            //         padding: {
 
-                    },
-                    fontFamily: "system-ui"
-                }, type, 3, group.map((item) => item.text).join(''), this.level != 1)
+            //         },
+            //         fontFamily: "system-ui"
+            //     }, type, 3, group.map((item) => item.text).join(''), this.level != 1)
+
+            // })
+            arr.forEach((value) => {
+                value.forEach((item) => {
+                    new DragText(this.scene, originX + item.index % rowMaxChat * 60, originY + item.row * 75, item.data.text, {
+                        fontSize: '55px',
+                        color: "#000000",
+                        backgroundColor: '#83A9E5',
+                        fontWeight: 'bold',
+                        padding: {
+                            x: this.level != 1 ? 0 : 5
+                        },
+                        fontFamily: "system-ui"
+                    }, type, 2, arr.map((value) => value.map((item) => item.data.text).join('')).join(''), this.level != 1)
+                })
 
             })
 

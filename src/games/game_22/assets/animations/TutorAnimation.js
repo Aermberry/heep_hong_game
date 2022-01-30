6c65e2842308039cb9c96cd98cdc091380572fe2
Salmon <yshpeng@liangzhicn.com>
2022-01-30 11:20:55 +0800

    doc: replace tutor atlas:1,2,4,5

--- src/games/game_22/assets/animations/TutorAnimation.js
+++ src/games/game_22/assets/animations/TutorAnimation.js
@@ -7,7 +7,7 @@ export const createTutorAnimations = (anims) => {
     anims.create({
         key: 'tutor01Animation',
         delay: 200,
-        frames: anims.generateFrameNames('tutor01Texture', { prefix: 'tutor_step_1', start: 0, end: 14, zeroPad: 4 }),
+        frames: anims.generateFrameNames('tutor01Texture', { prefix: 'tutor_step_1', start: 0, end: 29, zeroPad: 4 }),
         repeat: 0,
     });
 
@@ -28,14 +28,14 @@ export const createTutorAnimations = (anims) => {
     anims.create({
         key: 'tutor04Animation',
         delay: 200,
-        frames: anims.generateFrameNames('tutor04Texture', { prefix: 'tutor_step_4', start: 0, end: 16, zeroPad: 4 }),
+        frames: anims.generateFrameNames('tutor04Texture', { prefix: 'tutor_step_4', start: 0, end: 29, zeroPad: 4 }),
         repeat: 0,
     });
 
     anims.create({
         key: 'tutor05Animation',
         delay: 200,
-        frames: anims.generateFrameNames('tutor05Texture', { prefix: 'tutor_step_5', start: 0, end: 16, zeroPad: 4 }),
+        frames: anims.generateFrameNames('tutor05Texture', { prefix: 'tutor_step_5', start: 0, end: 29, zeroPad: 4 }),
         repeat: 0,
     });
 }

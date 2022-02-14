fe392340f80db8d2ff60b3800ec2b968946f44ba
Salmon <yshpeng@liangzhicn.com>
2022-02-14 14:14:50 +0800

    feat:game23 add gtag Event

--- src/games/game_23/index.js
+++ src/games/game_23/index.js
@@ -30,17 +30,20 @@ Phaser.Scenes.SceneManager.prototype.loadComplete = function (loader) {
 
 class Game23 extends Phaser.Game {
 
-    constructor(config, urlParams) {
+    constructor(config, urlParams,gtag) {
         super(config);
-        console.log(urlParams)
         
+        this.globals = {
+            gtag: gtag,
+            gameStageIndex:urlParams
+        }
     }
 }
 
 
-function launch(urlParams) {
+function launch(urlParams,gtag) {
 
-    let game = new Game23(gameConfig, urlParams)
+    let game = new Game23(gameConfig, urlParams,gtag)
 
     return game
 }
--- src/games/game_23/scenes/GameScene.js
+++ src/games/game_23/scenes/GameScene.js
@@ -54,6 +54,8 @@ export default class GameScene extends BasicScene {
     create() {
 
         super.create();
+
+        this.sys.game.globals.gtag.event(`game_${this.sys.game.globals.gameStageIndex}_start`, { 'event_category': 'js_games', 'event_label': 'Game Start'})
         
         this.sound.stopAll();
 

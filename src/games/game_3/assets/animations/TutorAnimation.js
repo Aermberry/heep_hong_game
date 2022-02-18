0cd0a9dfb658c2a1d2b842de327aac70d43a0bf0
Salmon <sos2012happy@gmail.com>
2022-02-19 00:32:25 +0800

    feat: replace animation atlas

--- src/games/game_3/assets/animations/TutorAnimation.js
+++ src/games/game_3/assets/animations/TutorAnimation.js
@@ -6,27 +6,28 @@
 export const createTutorAnimation = (anims) => {
     anims.create({
         key: 'tutorAnimation',
-        delay: 200,
+        // delay: 200,
         frames: anims.generateFrameNames('tutorTexture', { prefix: 'tutor_', start: 0, end: 15, zeroPad: 4 }),
         frameRate: 24,
         repeat: -1
     });
     anims.create({
         key: 'tutorAnimation01',
-        delay: 200,
+        // delay: 200,
         frames: anims.generateFrameNames('tutorTexture01', { prefix: 'tutor_', start: 0, end: 39, zeroPad: 4 }),
         repeat: 0
     });
     anims.create({
         key: 'tutorAnimation02',
-        delay: 200,
-        frames: anims.generateFrameNames('tutorTexture02', { prefix: 'tutor_', start: 0, end: 39, zeroPad: 4 }),
+        // delay: 200,
+        frames: anims.generateFrameNames('tutorTexture02', { prefix: 'tutor_', start: 0, end: 0, zeroPad: 4 }),
         repeat: 0
     });
     anims.create({
         key: 'tutorAnimation03',
-        delay: 200,
-        frames: anims.generateFrameNames('tutorTexture03', { prefix: 'tutor_', start: 0, end: 39, zeroPad: 4 }),
+        // delay: 200,
+        frames: anims.generateFrameNames('tutorTexture03', { prefix: 'tutor_', start: 10, end: 39, zeroPad: 4 }),
+        // duration:5 00,
         repeat: 0
     });
 }
--- src/games/game_3/scenes/TutorScene.js
+++ src/games/game_3/scenes/TutorScene.js
@@ -3,7 +3,9 @@ import StartButton from "../components/StartButton";
 import BasicScene from "./BasicScene"
 import GameManager from "../components/GameManager";
 import GameSprite from "../phaser3_framework/object/GameSprite";
-import { createTutorAnimation } from "../assets/animations/TutorAnimation";
+import {
+    createTutorAnimation
+} from "../assets/animations/TutorAnimation";
 // import Phaser from "phaser";
 
 export default class TutorScene extends BasicScene {
@@ -23,31 +25,47 @@ export default class TutorScene extends BasicScene {
 
         createTutorAnimation(this.anims);
 
-        this.#paintScene();
+        this.paintScene();
 
         await this.gameManager.initGameData();
 
     }
 
 
-    #paintScene() {
+     paintScene() {
 
         let uiLayer = this.add.layer().setDepth(0);
 
-        const tutorSprite01 = new GameSprite(this, this.cameras.main.width / 2 - 300, this.cameras.main.height / 2 - 50, 'tutorTexture01');
-        const tutorSprite02 = new GameSprite(this, this.cameras.main.width / 2, this.cameras.main.height / 2, 'tutorTexture02');
-        const tutorSprite03 = new GameSprite(this, this.cameras.main.width / 2+390, this.cameras.main.height / 2-70, 'tutorTexture03').setScale(0.75);
-
         const exitProgressGameButton = new ExitProgressGameButton(this, 100, 120);
         const startButton = new StartButton(this, this.getColWidth(6), this.getRowHeight(10)).setScale(0.8);
+        
+      
 
-        tutorSprite01.play('tutorAnimation01');
-        tutorSprite02.play('tutorAnimation02');
-        tutorSprite03.play('tutorAnimation03');
-
-        uiLayer.add([this.buildBackground('backgroundTutorEnd'), tutorSprite02, tutorSprite01, tutorSprite03, exitProgressGameButton, startButton]);
+         uiLayer.add([this.buildBackground('backgroundTutorEnd'), exitProgressGameButton, startButton]);
+         
+          this.playTutorAnimation(uiLayer)
 
 
+    }
+
+   playTutorAnimation(layer) {
+        const tutorSprite01 = new GameSprite(this, this.cameras.main.width / 2 - 300, this.cameras.main.height / 2 - 50, 'tutorTexture01');
+        const tutorSprite02 = new GameSprite(this, this.cameras.main.width / 2, this.cameras.main.height / 2, 'tutorTexture02');
+        const tutorSprite03 = new GameSprite(this, this.cameras.main.width / 2 + 390, this.cameras.main.height / 2 - 70, 'tutorTexture03').setScale(0.75);
+
+        layer.add([tutorSprite02, tutorSprite01, tutorSprite03])
+
+        tutorSprite01.on('animationcomplete', () => {
+           tutorSprite02.play('tutorAnimation02');
+        })
+        tutorSprite02.on('animationcomplete', () => {
+             tutorSprite03.play('tutorAnimation03');
+        })
+        tutorSprite03.on('animationcomplete', () => {
+            tutorSprite01.play('tutorAnimation01');
+        })
+
+        tutorSprite01.play('tutorAnimation01')
     }
 
 

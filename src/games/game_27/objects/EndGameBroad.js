0c0e9c8cfd7521b1bc89292c74e578e074f9cf6f
lcheng <lcheng@liangzhicn.com>
2022-03-07 10:50:45 +0800

    fix game27 end_box

--- src/games/game_27/objects/EndGameBroad.js
+++ src/games/game_27/objects/EndGameBroad.js
@@ -7,7 +7,9 @@ export default class EndGameBroad extends Phaser.GameObjects.Container {
     constructor(scene, x, y, children) {
         super(scene, x, y, children);
 
-        let endBox = scene.add.image( 0, 0, 'end_box')
+        let endBox = scene.add.image( 0, 0, 'end_box').setScale(0.7)
+        endBox.width = endBox.displayWidth
+        endBox.height = endBox.displayHeight
 
         scene.anims.create({
             key: 'end_pic', 
@@ -17,7 +19,7 @@ export default class EndGameBroad extends Phaser.GameObjects.Container {
         });
 
 
-        let animate = scene.add.sprite(0, 0, 'end_pic');
+        let animate = scene.add.sprite(0, 0, 'end_pic').setScale(1.1);
 
         let retryBtn = new RetryBtn(scene, - endBox.width * 0.22, endBox.height * 0.35)
         let existBtn = new ExistBigBtn(scene, endBox.width * 0.22, endBox.height * 0.35)
